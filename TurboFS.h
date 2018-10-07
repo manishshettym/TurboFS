ce40aea9189d6f52c50c84cd53b087f6c45089bd
Man24ish <mmshetty.98@gmail.com>
2018-10-07 14:39:08 +0530

    2 functions updated

--- TurboFS.h
+++ TurboFS.h
@@ -11,10 +11,23 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+#include "structures.h"
 
-#define ROOT_INO 2
+#define ROOT_INODE 2
 #define ENTRY_DIR 2
 #define ENTRY_FILE 1
 #define ENTRY_BASE_SIZE 8
 
-static const int print_logs = 0;
+static const int print_logs = 1;
+
+
+
+
+static uint32 size_of_block;
+static uint32 size_of_fragment;
+static uint32 number_of_groups;
+static uint32 n_512k_blocks_per_group;
+
+static uint32 indirect_blocks_count;
+static uint32 d_indirect_blocks_count;
+static uint32 t_indirect_blocks_count;
--- structures.h
+++ structures.h
@@ -7,7 +7,56 @@
 #define NO_OF_DIRECT_BLOCKS 12
 
 
-struct t_inode
+typedef struct t_superblock
+{
+	uint32 s_inodes_count; //inode count
+	uint32 s_blocks_count; //no of blocks
+	uint32 s_free_block_count; // free blocks
+	uint32 s_free_inodes_count; // free inodes
+	uint32 s_first_data_block; 
+	uint32 s_log_block_size; //block size=4kb
+
+	uint32 s_blocks_per_group; //reqd to set no of groups
+	uint32 s_frags_per_group;
+	uint32 s_inodes_per_group;
+	uint32 s_mtime;
+	uint32 s_wtime;
+	
+
+	uint16 s_mnt_count;
+
+	
+	uint16 s_magic;
+
+	//figure out
+	uint16 s_state;
+	uint16 s_errors;
+	uint16 s_minor_rev_level; 
+	uint32 s_lastcheck;
+	uint32 s_checkinterval;
+	uint32 s_creator_os;
+	uint32 s_rev_level;
+
+	uint16 s_def_resuid;
+	uint16 s_def_resgid;
+
+	uint32 s_first_inode;
+	uint16 s_inode_size;
+	uint16 s_block_group_nr;
+
+	//figue out
+	uint32 s_feature_compat;
+	uint32 s_feature_incompat;
+	uint32 s_feature_ro_compat;
+	uint16 s_uuid;
+	uint16 s_pad;
+	uint32 s_reserved[229];
+
+
+}superblock;
+
+
+typedef struct t_inode
 {
 	uint16 i_mode;// represents whether directory OR file
 	uint16 i_uid;
@@ -22,7 +71,7 @@ struct t_inode
 	uint32 i_dtime; //deleted
 
 	
-	uint16 i_links_count; //count of links
+	uint16 i_links_count; //count of links (st_nlink)
 	uint32 i_blocks; //count of blocks
 	uint32 i_flags; //flags for the inode
 
@@ -41,4 +90,40 @@ struct t_inode
 	uint32 i_osd2_2;
 	uint32 i_osd2_3;*/
 
-}
+}inode;
+
+typedef struct t_block_group_descriptor{
+	uint32 bg_block_bitmap;
+	uint32 bg_inode_bitmap;
+	uint32 bg_inode_table;
+	uint16 bg_free_blocks_count;
+	uint16 bg_free_inodes_count;
+	uint16 bg_used_dirs_count;
+	uint16 bg_pad;
+	uint32 bg_reserved1;
+	uint32 bg_reserved2;
+	uint32 bg_reserved3;
+}bg_descriptor;
+
+
+typedef struct t_dir_entry
+{
+	uint32	inode;			/* Inode number */
+	uint16	rec_len;		/* Directory entry length */
+	uint8	name_len;		/* Name length */
+	uint8	file_type;
+	char	name[MAX_NAME_LEN];	/* File name */
+	uint32 block_number;
+	uint32 offset;
+}dir_ent;
+
+
+typedef struct t_file_handle
+{
+	uint32 f_inode;
+	uint32 f_size;
+	uint32 f_blocks_count;
+}file_handle;
+
+
+
